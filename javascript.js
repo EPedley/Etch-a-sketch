d302cfcb5f0b7fbe86729b05bbeb900bedd48bf6
EPedley <lizzypedley@gmail.com>
2023-03-01 16:18:19 +0000

    Added the JS to create 16 divs in the grid element that flex

---
+++ javascript.js
@@ -0,0 +1,29 @@
+//function to decide grid layout
+
+function setGrid() {
+    //document.getElementById("grid").innerHTML = "Hello World";
+
+    for (let i = 0; i < 16; i++) {
+        document.getElementById("grid").innerHTML += "<div></div>"
+    }
+
+    //const grid = document.querySelector("#grid");
+
+    //const div = document.createElement("div");
+    
+    //for (let i = 0; i < 16; i++) {
+    //    grid.appendChild(div);
+    //}
+    
+
+    //grid.innerHTML = <div></div>;
+}
+
+
+//listen to grid button and begin grid layout function when clicked
+const gridButton = document.getElementById("grid-button");
+
+//remove 16 when done
+gridButton.addEventListener("click", setGrid(16));
+
+
